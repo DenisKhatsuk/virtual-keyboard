a9d7b561a9b5e09c9b3b7ba682fd95e5c6d55e54
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-04-07 15:36:13 +0300

    feat: add language switch

--- js/script.js
+++ js/script.js
@@ -5,74 +5,74 @@ const Keyboard = {
   elements: {
     main: null,
     keysLayout: [ 
-      {keyCode: 'Backquote', eng: '`', engShift: '~', ru: 'ё'}, 
-      {keyCode: 'Digit1', eng: '1', engShift: '!'}, 
-      {keyCode: 'Digit2', eng: '2', engShift: '@'}, 
-      {keyCode: 'Digit3', eng: '3', engShift: '#'}, 
-      {keyCode: 'Digit4', eng: '4', engShift: '$'}, 
-      {keyCode: 'Digit5', eng: '5', engShift: '%'}, 
-      {keyCode: 'Digit6', eng: '6', engShift: '^'}, 
-      {keyCode: 'Digit7', eng: '7', engShift: '&'}, 
-      {keyCode: 'Digit8', eng: '8', engShift: '*'}, 
-      {keyCode: 'Digit9', eng: '9', engShift: '('}, 
-      {keyCode: 'Digit0', eng: '0', engShift: ')'}, 
-      {keyCode: 'Minus', eng: '-', engShift: '_'}, 
-      {keyCode: 'Equal', eng: '=', engShift: '+'}, 
-      {keyCode: 'Backspace', eng: 'Backspace', engShift: 'none'}, 
+      {keyCode: 'Backquote', eng: '`', engShift: '~', ru: 'ё', ruShift: 'Ё'}, 
+      {keyCode: 'Digit1', eng: '1', engShift: '!', ru: 'none', ruShift: '!'}, 
+      {keyCode: 'Digit2', eng: '2', engShift: '@', ru: 'none', ruShift: '"'}, 
+      {keyCode: 'Digit3', eng: '3', engShift: '#', ru: 'none', ruShift: '№'}, 
+      {keyCode: 'Digit4', eng: '4', engShift: '$', ru: 'none', ruShift: ';'}, 
+      {keyCode: 'Digit5', eng: '5', engShift: '%', ru: 'none', ruShift: '%'}, 
+      {keyCode: 'Digit6', eng: '6', engShift: '^', ru: 'none', ruShift: ':'}, 
+      {keyCode: 'Digit7', eng: '7', engShift: '&', ru: 'none', ruShift: '?'}, 
+      {keyCode: 'Digit8', eng: '8', engShift: '*', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'Digit9', eng: '9', engShift: '(', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'Digit0', eng: '0', engShift: ')', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'Minus', eng: '-', engShift: '_', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'Equal', eng: '=', engShift: '+', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'Backspace', eng: 'Backspace', engShift: 'none', ru: 'none', ruShift: 'none'}, 
       {eng:'\n'},
-      {keyCode: 'Tab', eng: 'Tab', engShift: 'none'}, 
-      {keyCode: 'KeyQ', eng: 'q', engShift: 'Q'}, 
-      {keyCode: 'KeyW', eng: 'w', engShift: 'W'}, 
-      {keyCode: 'KeyE', eng: 'e', engShift: 'E'}, 
-      {keyCode: 'KeyR', eng: 'r', engShift: 'R'}, 
-      {keyCode: 'KeyT', eng: 't', engShift: 'T'}, 
-      {keyCode: 'KeyY', eng: 'y', engShift: 'Y'}, 
-      {keyCode: 'KeyU', eng: 'u', engShift: 'U'}, 
-      {keyCode: 'KeyI', eng: 'i', engShift: 'I'}, 
-      {keyCode: 'KeyO', eng: 'o', engShift: 'O'}, 
-      {keyCode: 'KeyP', eng: 'p', engShift: 'P'}, 
-      {keyCode: 'BracketLeft', eng: '{', engShift: '{'}, 
-      {keyCode: 'BracketRight', eng: ']', engShift: '}'}, 
-      {keyCode: 'Backslash', eng: '\\', engShift: '|'}, 
-      {keyCode: 'Delete', eng: 'Del', engShift: 'none'}, 
+      {keyCode: 'Tab', eng: 'Tab', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'KeyQ', eng: 'q', engShift: 'Q', ru: 'й', ruShift: 'Й'}, 
+      {keyCode: 'KeyW', eng: 'w', engShift: 'W', ru: 'ц', ruShift: 'Ц'}, 
+      {keyCode: 'KeyE', eng: 'e', engShift: 'E', ru: 'у', ruShift: 'У'}, 
+      {keyCode: 'KeyR', eng: 'r', engShift: 'R', ru: 'к', ruShift: 'К'}, 
+      {keyCode: 'KeyT', eng: 't', engShift: 'T', ru: 'е', ruShift: 'Е'}, 
+      {keyCode: 'KeyY', eng: 'y', engShift: 'Y', ru: 'н', ruShift: 'Н'}, 
+      {keyCode: 'KeyU', eng: 'u', engShift: 'U', ru: 'г', ruShift: 'Г'}, 
+      {keyCode: 'KeyI', eng: 'i', engShift: 'I', ru: 'ш', ruShift: 'Ш'}, 
+      {keyCode: 'KeyO', eng: 'o', engShift: 'O', ru: 'щ', ruShift: 'Щ'}, 
+      {keyCode: 'KeyP', eng: 'p', engShift: 'P', ru: 'з', ruShift: 'З'}, 
+      {keyCode: 'BracketLeft', eng: '{', engShift: '{', ru: 'х', ruShift: 'Х'}, 
+      {keyCode: 'BracketRight', eng: ']', engShift: '}', ru: 'ъ', ruShift: 'Ъ'}, 
+      {keyCode: 'Backslash', eng: '\\', engShift: '|', ru: 'none', ruShift: '/'}, 
+      {keyCode: 'Delete', eng: 'Del', engShift: 'none', ru: 'none', ruShift: 'none'}, 
       {eng: '\n'},
-      {keyCode: 'CapsLock', eng: 'CapsLock', engShift: 'none'}, 
-      {keyCode: 'KeyA', eng: 'a', engShift: 'A'}, 
-      {keyCode: 'KeyS', eng: 's', engShift: 'S'}, 
-      {keyCode: 'KeyD', eng: 'd', engShift: 'D'}, 
-      {keyCode: 'KeyF', eng: 'f', engShift: 'F'}, 
-      {keyCode: 'KeyG', eng: 'g', engShift: 'G'}, 
-      {keyCode: 'KeyH', eng: 'h', engShift: 'H'}, 
-      {keyCode: 'KeyJ', eng: 'j', engShift: 'J'}, 
-      {keyCode: 'KeyK', eng: 'k', engShift: 'K'}, 
-      {keyCode: 'KeyL', eng: 'l', engShift: 'L'}, 
-      {keyCode: 'Semicolon', eng: ';', engShift: ':'}, 
-      {keyCode: 'Quote', eng: '\'', engShift: '"'}, 
-      {keyCode: 'Enter', eng: 'Enter', engShift: 'none'}, 
+      {keyCode: 'CapsLock', eng: 'CapsLock', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'KeyA', eng: 'a', engShift: 'A', ru: 'ф', ruShift: 'Ф'}, 
+      {keyCode: 'KeyS', eng: 's', engShift: 'S', ru: 'ы', ruShift: 'Ы'}, 
+      {keyCode: 'KeyD', eng: 'd', engShift: 'D', ru: 'в', ruShift: 'В'}, 
+      {keyCode: 'KeyF', eng: 'f', engShift: 'F', ru: 'а', ruShift: 'А'}, 
+      {keyCode: 'KeyG', eng: 'g', engShift: 'G', ru: 'п', ruShift: 'П'}, 
+      {keyCode: 'KeyH', eng: 'h', engShift: 'H', ru: 'р', ruShift: 'Р'}, 
+      {keyCode: 'KeyJ', eng: 'j', engShift: 'J', ru: 'о', ruShift: 'О'}, 
+      {keyCode: 'KeyK', eng: 'k', engShift: 'K', ru: 'л', ruShift: 'Л'}, 
+      {keyCode: 'KeyL', eng: 'l', engShift: 'L', ru: 'д', ruShift: 'Д'}, 
+      {keyCode: 'Semicolon', eng: ';', engShift: ':', ru: 'ж', ruShift: 'Ж'}, 
+      {keyCode: 'Quote', eng: '\'', engShift: '"', ru: 'э', ruShift: 'Э'}, 
+      {keyCode: 'Enter', eng: 'Enter', engShift: 'none', ru: 'none', ruShift: 'none'}, 
       {eng: '\n'},
-      {keyCode: 'ShiftLeft', eng: 'Shift', engShift: 'none'}, 
-      {keyCode: 'KeyZ', eng: 'z', engShift: 'Z'}, 
-      {keyCode: 'KeyX', eng: 'x', engShift: 'X'}, 
-      {keyCode: 'KeyC', eng: 'c', engShift: 'C'}, 
-      {keyCode: 'KeyV', eng: 'v', engShift: 'V'}, 
-      {keyCode: 'KeyB', eng: 'b', engShift: 'B'}, 
-      {keyCode: 'KeyN', eng: 'n', engShift: 'N'}, 
-      {keyCode: 'KeyM', eng: 'm', engShift: 'M'}, 
-      {keyCode: 'Comma', eng: ',', engShift: '<'}, 
-      {keyCode: 'Period', eng: '.', engShift: '>'}, 
-      {keyCode: 'Slash', eng: '/', engShift: '?'}, 
-      {keyCode: 'ArrowUp', eng: '▲', engShift: 'none'}, 
-      {keyCode: 'ShiftRight', eng: 'Shift', engShift: 'none'}, 
+      {keyCode: 'ShiftLeft', eng: 'Shift', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'KeyZ', eng: 'z', engShift: 'Z', ru: 'я', ruShift: 'Я'}, 
+      {keyCode: 'KeyX', eng: 'x', engShift: 'X', ru: 'ч', ruShift: 'Ч'}, 
+      {keyCode: 'KeyC', eng: 'c', engShift: 'C', ru: 'с', ruShift: 'С'}, 
+      {keyCode: 'KeyV', eng: 'v', engShift: 'V', ru: 'м', ruShift: 'М'}, 
+      {keyCode: 'KeyB', eng: 'b', engShift: 'B', ru: 'и', ruShift: 'И'}, 
+      {keyCode: 'KeyN', eng: 'n', engShift: 'N', ru: 'т', ruShift: 'Т'}, 
+      {keyCode: 'KeyM', eng: 'm', engShift: 'M', ru: 'ь', ruShift: 'Ь'}, 
+      {keyCode: 'Comma', eng: ',', engShift: '<', ru: 'б', ruShift: 'Б'}, 
+      {keyCode: 'Period', eng: '.', engShift: '>', ru: 'ю', ruShift: 'Ю'}, 
+      {keyCode: 'Slash', eng: '/', engShift: '?', ru: '.', ruShift: ','}, 
+      {keyCode: 'ArrowUp', eng: '▲', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'ShiftRight', eng: 'Shift', engShift: 'none', ru: 'none', ruShift: 'none'}, 
       {eng: '\n'},
-      {keyCode: 'ControlLeft', eng: 'Ctrl', engShift: 'none'}, 
-      {keyCode: 'MetaLeft', eng: 'Win', engShift: 'none'}, 
-      {keyCode: 'AltLeft', eng: 'Alt', engShift: 'none'}, 
-      {keyCode: 'Space', eng: 'Space', engShift: 'none'}, 
-      {keyCode: 'AltRight', eng: 'Alt', engShift: 'none'}, 
-      {keyCode: 'ArrowLeft', eng: '◄', engShift: 'none'}, 
-      {keyCode: 'ArrowDown', eng: '▼', engShift: 'none'}, 
-      {keyCode: 'ArrowRight', eng: '►', engShift: 'none'}, 
-      {keyCode: 'ControlRight', eng: 'Ctrl', engShift: 'none'}
+      {keyCode: 'ControlLeft', eng: 'Ctrl', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'MetaLeft', eng: 'Win', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'AltLeft', eng: 'Alt', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'Space', eng: 'Space', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'AltRight', eng: 'Alt', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'ArrowLeft', eng: '◄', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'ArrowDown', eng: '▼', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'ArrowRight', eng: '►', engShift: 'none', ru: 'none', ruShift: 'none'}, 
+      {keyCode: 'ControlRight', eng: 'Ctrl', engShift: 'none', ru: 'none', ruShift: 'none'}
     ],
     keys: null,
   },
@@ -80,7 +80,8 @@ const Keyboard = {
   properties: {
     capsLock: false,
     shift: false,
-    language: 'RU',
+    language: 'ENG',
+    languageSwitcher: false,
   },
 
   init() {
@@ -108,6 +109,8 @@ const Keyboard = {
           key.setAttribute('data-code', el.keyCode);
           key.setAttribute('data-eng', el.eng);
           key.setAttribute('data-engShift', el.engShift);
+          key.setAttribute('data-ru', el.ru);
+          key.setAttribute('data-ruShift', el.ruShift);
           key.appendChild(keyContent);
           fragment.appendChild(key);
           break;
@@ -120,6 +123,8 @@ const Keyboard = {
           key.setAttribute('data-code', el.keyCode);
           key.setAttribute('data-eng', el.eng);
           key.setAttribute('data-engShift', el.engShift);
+          key.setAttribute('data-ru', el.ru);
+          key.setAttribute('data-ruShift', el.ruShift);
           key.appendChild(keyContent);
           fragment.appendChild(key);
           break;
@@ -130,6 +135,8 @@ const Keyboard = {
           key.setAttribute('data-code', el.keyCode);
           key.setAttribute('data-eng', el.eng);
           key.setAttribute('data-engShift', el.engShift);
+          key.setAttribute('data-ru', el.ru);
+          key.setAttribute('data-ruShift', el.ruShift);
           key.appendChild(keyContent);
           fragment.appendChild(key);
           break;
@@ -140,6 +147,8 @@ const Keyboard = {
           key.setAttribute('data-code', el.keyCode);
           key.setAttribute('data-eng', el.eng);
           key.setAttribute('data-engShift', el.engShift);
+          key.setAttribute('data-ru', el.ru);
+          key.setAttribute('data-ruShift', el.ruShift);
           key.appendChild(keyContent);
           fragment.appendChild(key);
           break;
@@ -150,6 +159,8 @@ const Keyboard = {
           key.setAttribute('data-code', el.keyCode);
           key.setAttribute('data-eng', el.eng);
           key.setAttribute('data-engShift', el.engShift);
+          key.setAttribute('data-ru', el.ru);
+          key.setAttribute('data-ruShift', el.ruShift);
           key.appendChild(keyContent);
           fragment.appendChild(key);
           break;
@@ -235,7 +246,6 @@ const Keyboard = {
       if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
         if (!_this.properties.shift) {
           _this.properties.shift = true;
-          console.log('Shift active');
           _this.elements.keys.forEach(function(el) {
             if (el.getAttribute('data-engshift') !== 'none') {
               el.textContent = _this.properties.capsLock ? el.getAttribute('data-engshift').toLowerCase() : el.getAttribute('data-engshift');
@@ -243,23 +253,53 @@ const Keyboard = {
           });
         } 
       }
+      //language switch
+      if(event.ctrlKey && event.code === 'AltLeft') {
+        if (!_this.properties.languageSwitch) _this.properties.languageSwitch = true;
+      }
     });
     document.addEventListener('keyup', function(event) {
       _this.elements.keys.forEach(function(el) {
         if (el.getAttribute('data-code') === event.code && event.code !== 'CapsLock') el.classList.remove('keyboard__key_active');
       });
       //shift handler
-      if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
-        _this.properties.shift = false;
-      }
-      _this.elements.keys.forEach(function(el) {
-        if (el.getAttribute('data-engshift') !== 'none') {
-          el.textContent = _this.properties.capsLock ? el.getAttribute('data-eng').toUpperCase() : el.getAttribute('data-eng');
-        }
+      // if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+      //   _this.properties.shift = false;
+      // }
+      // _this.elements.keys.forEach(function(el) {
+      //   if (el.getAttribute('data-engshift') !== 'none') {
+      //     el.textContent = _this.properties.capsLock ? el.getAttribute('data-eng').toUpperCase() : el.getAttribute('data-eng');
+      //   }
 
-      });
+      // });
+      //language switch
+      if(event.altKey) {
+        if (_this.properties.languageSwitch) {
+          _this.properties.language = _this.properties.language === 'ENG' ? 'RU' : 'ENG';
+          _this.properties.languageSwitch = false;
+        }
+        _this.setKeysValue();
+      }
     });
-  }
+  },
+
+  setKeysValue() {
+    let _this = this;
+    let lang;
+    let shift;
+    if (_this.properties.language === 'ENG') {
+      lang = 'data-eng';
+      shift = 'data-engshift';
+    } else {
+      lang = 'data-ru';
+      shift = 'data-rushift';
+    } 
+    _this.elements.keys.forEach(function(el) {
+      if (el.getAttribute(lang) !== 'none') {
+        el.textContent = el.getAttribute(lang);
+      }
+    });
+  },
 
 }
 
